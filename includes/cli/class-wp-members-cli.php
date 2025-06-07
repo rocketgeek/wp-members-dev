9a0414a1cd68013dd82e59d94d825c3a9544d4b0
rocketgeek <support@rocketgeek.com>
2025-06-07 10:16:23 -0400

    improve cli commands

--- includes/cli/class-wp-members-cli.php
+++ includes/cli/class-wp-members-cli.php
@@ -8,37 +8,56 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 	class WP_Members_CLI {
 		
 		/**
-		 * Object getter.
+		 * Gets that status or block value of a post or post setting.
 		 * 
 		 * ## OPTIONS
 		 *
-		 * <what>
-		 * : What to get? <block_val|status|hidden|>
+		 * <block_val|status|hidden>
+		 * : What to get? 
 		 *
-		 * [--id=<post_id>]
+		 * --id=<post_id|post_slug>
 		 * : Post ID
 		 */
 		public function get( $args, $assoc_args ) {
-			
-			$post_id = $assoc_args['id'];
+
+			if ( is_numeric( $assoc_args['id'] ) ) {
+				$post_id = $assoc_args['id'];
+			} else {
+				$post = get_page_by_path( $assoc_args['id'], OBJECT );
+				if ( $post ) {
+					$post_id = $post->ID;
+				} else {
+					WP_CLI::error( $assoc_args['id'] . ' is not a valid post' );
+				}
+			}
 			
 			switch ( $args[0] ) {
 				
 				case "block_val":
-					WP_CLI::line( 'post block setting:' . ' ' . wpmem_get_block_setting( $post_id ) );
+					$block_setting = wpmem_get_block_setting( $post_id );
+					switch( $block_setting ) {
+						case '1':
+							$block_text = 'restricted';
+							break;
+						case '2':
+							$block_text = 'hidden';
+							break;
+						case '0':
+						case false:
+							$block_text = 'unrestricted';
+							break;
+					}
+					WP_CLI::line( 'post restriction setting:' . ' ' . $block_text );
 					break;
 					
 				case "status":
-					if ( false === get_post_status ( $post_id ) ) {
-						/* translators: %s is the placeholder for the post ID, do not remove it. */
+					if ( false == get_post_status( $post_id ) ) {
 						WP_CLI::error( sprintf( 'No post id %d exists. Try wp post list', $post_id ) );
 					}
-					if ( true === wpmem_is_hidden( $post_id ) ) {
-						/* translators: %s is the placeholder for the post ID, do not remove it. */
+					if ( true == wpmem_is_hidden( $post_id ) ) {
 						$line = sprintf( 'post %s is hidden', $post_id );
 					} else {
-						/* translators: %s is the placeholder for the post ID, do not remove it. */
-						$line = ( wpmem_is_blocked( $post_id ) ) ? sprintf( 'post %s is blocked', $post_id ) : sprintf( 'post %s is not blocked', $post_id );
+						$line = ( wpmem_is_blocked( $post_id ) ) ? sprintf( 'post %s is restricted', $post_id ) : sprintf( 'post %s is not restricted', $post_id );
 					}
 					WP_CLI::line( $line );
 					break;
@@ -70,43 +89,40 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 		 *
 		 * ## OPTIONS
 		 *
-		 * <what>
-		 * : What to set (status).
-		 *
-		 * [--id=<post_id>]
+		 * --id=<post_id>
 		 * : Post ID to set property for.
 		 *
-		 * [--status=<unblock|unrestrict|hide|block|restrict>]
+		 * --status=<unblock|unrestrict|hide|block|restrict>
 		 * : The status to set.
+		 * ---
+		 * options:
+		 *    - restrict
+		 *    - block
+		 *    - unrestrict
+		 *    - unblock
+		 *    - hide
 		 *
 		 * @since 3.3.5
 		 */
 		public function set( $args, $assoc_args ) {
 			
 			$post_id = $assoc_args['post_id'];
-			
-			switch( $args[0] ) {
-				
-				case 'status':
-					switch( $assoc_args['status'] ) {
-						case 'unblock':
-						case 'unrestrict':
-							$val = 0; $line = 'unrestricted';
-							break;
-						case 'hide':
-							$val = 2; $line = 'hidden';
-							break;
-						case 'block':
-						case 'restrict':
-						default;
-							$val = 1; $line = 'restricted';
-							break;
-					}
-					update_post_meta( $post_id, '_wpmem_block', $val );
-					/* translators: %s is a placeholder, do not remove it. */
-					WP_CLI::line( sprintf( 'Set post id %s as %s', $post_id, $line ) );
+			switch( $assoc_args['status'] ) {
+				case 'unblock':
+				case 'unrestrict':
+					$val = 0; $line = 'unrestricted';
+					break;
+				case 'hide':
+					$val = 2; $line = 'hidden';
+					break;
+				case 'block':
+				case 'restrict':
+				default;
+					$val = 1; $line = 'restricted';
 					break;
 			}
+			update_post_meta( $post_id, '_wpmem_block', $val );
+			WP_CLI::line( sprintf( 'Set post id %s as %s', $post_id, $line ) );
 		}
 		
 		/**
