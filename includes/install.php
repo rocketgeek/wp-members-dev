6147ca893c9f3441039692c60bc4e248453f5d82
rocketgeek <support@rocketgeek.com>
2025-09-25 19:11:21 -0400

    prepare for 3.5.5

--- includes/install.php
+++ includes/install.php
@@ -714,7 +714,7 @@ class WP_Members_Installer {
 		global $wpmem;
 
 		$show_release_notes = true;
-		$release_notes_link = "https://rocketgeek.com/release-announcements/wp-members-3-5-3/";
+		$release_notes_link = "https://rocketgeek.com/release-announcements/wp-members-3-5-5/";
 
 		if ( 'new_install' == $wpmem->install_state ) {
 			$notice_heading = __( 'Thank you for installing WP-Members, the original WordPress membership plugin.', 'wp-members' );
--- wp-members.php
+++ wp-members.php
@@ -3,7 +3,7 @@
 Plugin Name: WP-Members
 Plugin URI:  https://rocketgeek.com
 Description: WP access restriction and user registration.  For more information on plugin features, refer to <a href="https://rocketgeek.com/plugins/wp-members/docs/">the online Users Guide</a>. A <a href="https://rocketgeek.com/plugins/wp-members/quick-start-guide/">Quick Start Guide</a> is also available. WP-Members(tm) is a trademark of butlerblog.com.
-Version:     3.5.4
+Version:     3.5.5
 Author:      Chad Butler
 Author URI:  https://butlerblog.com/
 Text Domain: wp-members
@@ -58,8 +58,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Initialize constants.
-define( 'WPMEM_VERSION',    '3.5.4' );
-define( 'WPMEM_DB_VERSION', '2.4.2' );
+define( 'WPMEM_VERSION',    '3.5.5' );
+define( 'WPMEM_DB_VERSION', '2.4.3' );
 define( 'WPMEM_PATH', plugin_dir_path( __FILE__ ) ); // @todo Fairly certain this is obsolete.
 
 // Initialize the plugin.
