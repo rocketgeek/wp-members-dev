8c689b305ff1d066ddea693e86e4125eb84353a1
rocketgeek <support@rocketgeek.com>
2025-10-23 15:59:31 -0400

    cli funcs for expires and time remaining

--- includes/cli/class-wp-members-cli-user.php
+++ includes/cli/class-wp-members-cli-user.php
@@ -404,6 +404,46 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 		}
 
 		/**
+		 * Displays a given users expiration date for a specific membership.
+		 * 
+		 * --membership=<meta_key>
+		 * : The membership to check.
+		 * 
+		 * --id=<user_id>
+		 * : The user ID to check.
+		 * 
+		 * [--format=<date_format>] 
+		 * : Date format to display (defaults to WP date format setting).
+		 */
+		public function expires( $args, $assoc_args ) {
+			$product_key = $assoc_args['membership'];
+			$user_id     = $assoc_args['id'];
+			$format      = ( isset( $assoc_args['format'] ) ) ? $assoc_args['format'] : get_option( 'date_format' );
+			$expires     = wpmem_get_user_expiration( $product_key, $user_id, $format );
+			WP_CLI::line( sprintf( 'User membership for %s expires %s.', wpmem_get_membership_name( $product_key ), $expires ) );
+		}
+
+		/**
+		 * Displays the user's remaining time for a specific membership.
+		 * 
+		 * --membership=<meta_key>
+		 * : The membership to check.
+		 * 
+		 * --id=<user_id>
+		 * : The user ID to check.
+		 * 
+		 * [--interval=<day|month>] 
+		 * : Date interval displayed as result (defaults to "days").
+		 */
+		public function remaining( $args, $assoc_args ) {
+			$product_key = $assoc_args['membership'];
+			$user_id     = $assoc_args['id'];
+			$interval    = ( isset( $assoc_args['interval'] ) ) ? $interval : 'days';
+			$remaining   = wpmem_get_user_time_remaining( $product_key, $user_id, $interval );
+			WP_CLI::line( sprintf( 'User %s has %s %s remaining for %s.', $user_id, $remaining, $interval, wpmem_get_membership_name( $product_key ) ) );
+		}
+
+		/**
 		 * Handles user detail display.
 		 *
 		 * @since 3.3.5
