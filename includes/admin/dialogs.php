22040a1de502218d641cd600ab51fecc578027f6
rocketgeek <support@rocketgeek.com>
2026-05-16 18:44:41 -0400

    apply recs from plugin check

--- includes/admin/dialogs.php
+++ includes/admin/dialogs.php
@@ -35,7 +35,7 @@ function wpmem_a_do_warnings( $did_update ) {
 	$dialogs = apply_filters( 'wpmem_dialogs', get_option( 'wpmembers_dialogs' ) ); 
 
 	if ( $did_update != false ) {?>
-		<div id="message" class="updated fade"><p><strong><?php echo $did_update; ?></strong></p></div><?php
+		<div id="message" class="updated fade"><p><strong><?php echo wp_kses( $did_update, 'post' ); ?></strong></p></div><?php
 	}
 
 	/*
@@ -80,15 +80,15 @@ function wpmem_a_do_warnings( $did_update ) {
 	
 	// If there is an active warning, display message about warnings.
 	if ( ! empty( $active_warnings ) ) {
-		$strong_msg = esc_html__( 'WP-Members Options', 'wp-members' ) . ': ' . esc_html__( 'You have active settings that are not recommended.', 'wp-members' );
-		$remain_msg = esc_html__( 'If you will not be changing these settings, you can turn off this warning message by checking the "Ignore warning messages" in the settings below.', 'wp-members' );
+		$strong_msg = __( 'WP-Members Options', 'wp-members' ) . ': ' . __( 'You have active settings that are not recommended.', 'wp-members' );
+		$remain_msg = __( 'If you will not be changing these settings, you can turn off this warning message by checking the "Ignore warning messages" in the settings below.', 'wp-members' );
 
-		echo '<div class="error"><p><strong>' . $strong_msg . '</strong></p><ul style="list-style:initial; margin:5px 20px">';
+		echo '<div class="error"><p><strong>' . esc_html( $strong_msg ) . '</strong></p><ul style="list-style:initial; margin:5px 20px">';
 		foreach ( $active_warnings as $warning ) {
-			echo $warning;
+			echo esc_html( $warning );
 		}
 		echo '</ul>';
-		echo '<p>' . $remain_msg  . '</p></div>';
+		echo '<p>' . esc_html( $remain_msg )  . '</p></div>';
 	}
 
 }
@@ -109,40 +109,43 @@ function wpmem_a_warning_msg( $msg ) {
 	switch ( $msg ) {
 
 	case 'users_can_register':
-		$strong_msg = esc_html__( 'Your WP settings allow anyone to register - this is not the recommended setting.', 'wp-members' );
-		$remain_msg = sprintf( esc_html__( 'You can %s change this here %s making sure the box next to "Anyone can register" is unchecked.', 'wp-members'), '<a href="options-general.php" target="_blank">', '</a>' );
-		$span_msg   = esc_html__( 'If you do not want users to register through wp-login.php, uncheck this option.', 'wp-members' );
+		$strong_msg = __( 'Your WP settings allow anyone to register - this is not the recommended setting.', 'wp-members' );
+		/* translators: placeholders here are for some an <a href> html tag. */
+		$remain_msg = sprintf( __( 'You can %s change this here %s making sure the box next to "Anyone can register" is unchecked.', 'wp-members'), '<a href="options-general.php" target="_blank">', '</a>' );
+		$span_msg   = __( 'If you do not want users to register through wp-login.php, uncheck this option.', 'wp-members' );
 		break;
 
 	case 'comment_registration':
-		$strong_msg = esc_html__( 'Your WP settings allow anyone to comment - this is not the recommended setting.', 'wp-members' );
-		$remain_msg = sprintf( esc_html__( 'You can %s change this here %s by checking the box next to "Users must be registered and logged in to comment."', 'wp-members' ), '<a href="options-discussion.php" target="_blank">', '</a>' );
-		$span_msg   = esc_html__( 'If you do not want non-registered users to comment, change this setting.', 'wp-members' );
+		$strong_msg = __( 'Your WP settings allow anyone to comment - this is not the recommended setting.', 'wp-members' );
+		/* translators: placeholders here are for some an <a href> html tag. */
+		$remain_msg = sprintf( __( 'You can %s change this here %s by checking the box next to "Users must be registered and logged in to comment."', 'wp-members' ), '<a href="options-discussion.php" target="_blank">', '</a>' );
+		$span_msg   = __( 'If you do not want non-registered users to comment, change this setting.', 'wp-members' );
 		break;
 
 	case 'rss_use_excerpt':
-		$strong_msg = esc_html__( 'Your WP settings allow full text rss feeds - this is not the recommended setting.', 'wp-members' );
-		$remain_msg = sprintf( esc_html__( 'You can %s change this here %s by changing "For each article in a feed, show" to "Summary."', 'wp-members' ), '<a href="options-reading.php" target="_blank">' , '</a>' );
-		$span_msg   = esc_html__( 'Full text feeds allow your protected content in an RSS reader.', 'wp-members' );
+		$strong_msg = __( 'Your WP settings allow full text rss feeds - this is not the recommended setting.', 'wp-members' );
+		/* translators: placeholders here are for some an <a href> html tag. */
+		$remain_msg = sprintf( __( 'You can %s change this here %s by changing "For each article in a feed, show" to "Summary."', 'wp-members' ), '<a href="options-reading.php" target="_blank">' , '</a>' );
+		$span_msg   = __( 'Full text feeds allow your protected content in an RSS reader.', 'wp-members' );
 		break;
 
 	case 'success':
-		$strong_msg = esc_html__( 'You have set WP-Members to hold registrations for approval', 'wp-members' );
-		$remain_msg = esc_html__( 'but you have not changed the default message for "Registration Completed" under "WP-Members Dialogs and Error Messages."  You should change this message to let users know they are pending approval.', 'wp-members' );
+		$strong_msg = __( 'You have set WP-Members to hold registrations for approval', 'wp-members' );
+		$remain_msg = __( 'but you have not changed the default message for "Registration Completed" under "WP-Members Dialogs and Error Messages."  You should change this message to let users know they are pending approval.', 'wp-members' );
 		break;
 
 	case 'wpmembers_captcha':
-		$strong_msg = esc_html__( 'You have turned on reCAPTCHA', 'wp-members');
-		$remain_msg = esc_html__( 'but you have not entered API keys.  You will need both a public and private key.  The CAPTCHA will not display unless a valid API key is included.', 'wp-members' );
+		$strong_msg = __( 'You have turned on reCAPTCHA', 'wp-members');
+		$remain_msg = __( 'but you have not entered API keys.  You will need both a public and private key.  The CAPTCHA will not display unless a valid API key is included.', 'wp-members' );
 		break;
 
 	}
 
 	if ( $span_msg ) {
-		$span_msg = ' [<span data-tooltip="' . $span_msg . '">why is this?</span>]';
+		$span_msg = ' [<span data-tooltip="' . esc_attr( $span_msg ) . '">why is this?</span>]';
 	}
 	
-	return '<li><strong>' . $strong_msg . '</strong> ' . $remain_msg . $span_msg . '</li>';
+	return '<li><strong>' . esc_html( $strong_msg ) . '</strong> ' . wp_kses( $remain_msg, wp_kses_allowed_html( 'post' ) ) . $span_msg . '</li>';
 
 }
 
@@ -162,19 +165,19 @@ function wpmem_a_meta_box() {
 		<h3><span>WP-Members Information</span></h3>
 		<div class="inside">
 
-			<p><strong><?php _e( 'Version:', 'wp-members' ); echo "&nbsp;" . $wpmem->version; ?></strong><br />
-				<a href="https://rocketgeek.com/plugins/wp-members/quick-start-guide/"><?php _e( 'Quick Start Guide', 'wp-members' ); ?></a><br />
-				<a href="https://rocketgeek.com/plugins/wp-members/docs/"><?php _e( 'Online User Guide', 'wp-members' ); ?></a><br />
-				<a href="https://rocketgeek.com/plugins/wp-members/docs/faqs/"><?php _e( 'FAQs', 'wp-members' ); ?></a>
+			<p><strong><?php esc_html_e( 'Version:', 'wp-members' ); echo "&nbsp;" . esc_html( $wpmem->version ); ?></strong><br />
+				<a href="https://rocketgeek.com/plugins/wp-members/quick-start-guide/"><?php esc_html_e( 'Quick Start Guide', 'wp-members' ); ?></a><br />
+				<a href="https://rocketgeek.com/plugins/wp-members/docs/"><?php esc_html_e( 'Online User Guide', 'wp-members' ); ?></a><br />
+				<a href="https://rocketgeek.com/plugins/wp-members/docs/faqs/"><?php esc_html_e( 'FAQs', 'wp-members' ); ?></a>
 			<?php if( ! defined( 'WPMEM_REMOVE_ATTR' ) ) { ?>
 				<br /><br /><a href="https://rocketgeek.com/about/site-membership-subscription/">Find out how to get access</a> to WP-Members private members forum, premium code snippets, tutorials, and add-on modules!
 			<?php } ?>
 			</p>
 
 			<p><i>
-			<?php _e( 'Thank you for using WP-Members', 'wp-members' ); ?>&trade;!<br /><br />
-			<?php _e( 'A plugin developed by', 'wp-members' ); ?>&nbsp;<a href="http://butlerblog.com">Chad Butler</a><br />
-			<?php _e( 'Follow', 'wp-members' ); ?> ButlerBlog: <a href="http://feeds.butlerblog.com/butlerblog" target="_blank">RSS</a> | <a href="http://www.twitter.com/butlerblog" target="_blank">Twitter</a><br />
+			<?php esc_html_e( 'Thank you for using WP-Members', 'wp-members' ); ?>&trade;!<br /><br />
+			<?php esc_html_e( 'A plugin developed by', 'wp-members' ); ?>&nbsp;<a href="http://butlerblog.com">Chad Butler</a><br />
+			<?php esc_html_e( 'Follow', 'wp-members' ); ?> ButlerBlog: <a href="http://feeds.butlerblog.com/butlerblog" target="_blank">RSS</a> | <a href="http://www.twitter.com/butlerblog" target="_blank">Twitter</a><br />
 			Copyright &copy; 2006-<?php echo date("Y"); ?><br /><br />
 			Premium support and installation service <a href="https://rocketgeek.com/about/site-membership-subscription/">available at rocketgeek.com</a>.
 			</i></p>
@@ -191,7 +194,7 @@ function wpmem_a_meta_box() {
 function wpmem_a_rss_box() {
 
 	?><div class="postbox">
-		<h3><span><?php _e( 'Latest from RocketGeek', 'wp-members' ); ?></span></h3>
+		<h3><span><?php esc_html_e( 'Latest from RocketGeek', 'wp-members' ); ?></span></h3>
 		<div class="inside"><?php
 		wp_widget_rss_output( array(
 			'url'          => 'https://rocketgeek.com/feed/',  //put your feed URL here
@@ -212,7 +215,7 @@ function wpmem_a_rss_box() {
  */
 function wpmem_a_rating_box() {
 	?><div class="postbox">
-		<h3><?php _e( 'Like WP-Members?', 'wp-members' ); ?></h3>
+		<h3><?php esc_html_e( 'Like WP-Members?', 'wp-members' ); ?></h3>
 		<div class="inside"><?php echo sprintf( esc_html__( 'If you like WP-Members please give it a %s&#9733;&#9733;&#9733;&#9733;&#9733;%s rating. Thanks!!', 'wp-members' ), '<a href="https://wordpress.org/support/plugin/wp-members/reviews?rate=5#new-post">', '</a>' ); ?></div>
 	</div><?php
 }
@@ -222,9 +225,10 @@ function wpmem_a_rating_box() {
  * Add the dashboard widget.
  *
  * @since 2.8.0
+ * @since 3.5.7 Changed function name from butlerblog_dashboard_widget to wpmem_dashboard_widget.
  */
-function butlerblog_dashboard_widget() {
-	wp_add_dashboard_widget( 'dashboard_custom_feed', esc_html__( 'Latest from ButlerBlog', 'wp-members' ), 'butlerblog_feed_output' );
+function wpmem_dashboard_widget() {
+	wp_add_dashboard_widget( 'dashboard_custom_feed', esc_html__( 'Latest from ButlerBlog', 'wp-members' ), 'wpmem_feed_output' );
 }
 
 
@@ -232,8 +236,9 @@ function butlerblog_dashboard_widget() {
  * Output the rss feed for the dashboard widget.
  *
  * @since 2.8.0
+ * @since 3.5.7 Changed function name from butlerblog_feed_output to wpmem_feed_output.
  */
-function butlerblog_feed_output() {
+function wpmem_feed_output() {
 	echo '<div class="rss-widget">';
 	wp_widget_rss_output( array(
 		'url'          => 'https://feeds.feedburner.com/butlerblog',
