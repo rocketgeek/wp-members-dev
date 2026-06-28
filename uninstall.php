9206351297046e6c9638c60fbbd39656c9ed5dac
rocketgeek <support@rocketgeek.com>
2026-06-28 18:26:28 -0400

    avoid global var issues

--- uninstall.php
+++ uninstall.php
@@ -30,14 +30,14 @@ if ( WP_UNINSTALL_PLUGIN ) {
 	if ( is_multisite() ) {
 
 		global $wpdb;
-		$blog_ids = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
-		$original_blog_id = get_current_blog_id();
+		$wpmem_blog_ids = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
+		$wpmem_orig_blog_id = get_current_blog_id();
 
-		foreach ( $blog_ids as $blog_id ) {
+		foreach ( $wpmem_blog_ids as $blog_id ) {
 			switch_to_blog( $blog_id );
 			wpmem_uninstall_options(); 
 		}
-		switch_to_blog( $original_blog_id );
+		switch_to_blog( $wpmem_orig_blog_id );
 	
 	} else {
 		wpmem_uninstall_options();
